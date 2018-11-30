64c5e27bab910eb9d0730ecafc7933ed4c6963f6
Damyan Ivanov <dam+mobileledger@ktnx.net>
2018-11-30 18:15:29 +0000

    replace add account floating action button with a menu item

--- app/src/main/java/net/ktnx/mobileledger/NewTransactionActivity.java
+++ app/src/main/java/net/ktnx/mobileledger/NewTransactionActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.Objects;
@@ -30,11 +32,23 @@ public class NewTransactionActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.new_transaction, menu);
+
+        return true;
+    }
+
     public void pickTransactionDate(View view) {
         DialogFragment picker = new DatePickerFragment();
         picker.show(getSupportFragmentManager(), "datePicker");
 //        Snackbar.make(view, "Date editing not yet ready", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show();
+    }
+
+    public void addTransactionAccountFromMenu(MenuItem item) {
+        Snackbar.make(getCurrentFocus(), "Not implemented yet", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
 }
