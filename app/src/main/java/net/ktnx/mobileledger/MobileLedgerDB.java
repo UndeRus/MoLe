65e1c4a034f5f08db9117f7211ca924e2e293f8e
Damyan Ivanov <dam+mobileledger@ktnx.net>
2018-12-02 10:54:34 +0000

    helper class for DB stuff

---
+++ app/src/main/java/net/ktnx/mobileledger/MobileLedgerDB.java
@@ -0,0 +1,24 @@
+package net.ktnx.mobileledger;
+
+import android.database.sqlite.SQLiteDatabase;
+
+class MobileLedgerDB {
+    static final String DATABASE_NAME = "accounts";
+    private static String db_filename;
+    static SQLiteDatabase db;
+
+    public static String getDb_filename() {
+        return db_filename;
+    }
+
+    public static void setDb_filename(String db_filename) {
+        MobileLedgerDB.db_filename = db_filename;
+    }
+
+    public static void initDB() {
+        db = SQLiteDatabase.openOrCreateDatabase( db_filename, null );
+
+        db.execSQL("create table if not exists accounts(name varchar);");
+        db.execSQL("create index if not exists idx_accounts_name on accounts(name);");
+    }
+}
