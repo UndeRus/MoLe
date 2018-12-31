d14edf52b78a295408db62145dd321414fea8513
Damyan Ivanov <dam+mobileledger@ktnx.net>
2018-12-31 05:35:23 +0000

    more main activity rename, cleanup of unused stuff

--- app/src/main/java/net/ktnx/mobileledger/ui/activity/MainActivity.java
+++ app/src/main/java/net/ktnx/mobileledger/ui/activity/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_summary);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
