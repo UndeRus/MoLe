ae668e7470adf8eefb0fd2bb5905f9c6ebebfc36
Damyan Ivanov <dam+mobileledger@ktnx.net>
2019-03-11 20:30:51 +0200

    spurious setting
    no need, if there is a change, the next line will make it visible via the
    observable value

--- app/src/main/java/net/ktnx/mobileledger/ui/activity/MainActivity.java
+++ app/src/main/java/net/ktnx/mobileledger/ui/activity/MainActivity.java
@@ -91,7 +91,6 @@ public class MainActivity extends ProfileThemedActivity {
     protected void onStart() {
         super.onStart();
 
-        Data.lastUpdateDate.set(null);
         updateLastUpdateTextFromDB();
         Date lastUpdate = Data.lastUpdateDate.get();
 
