fb2d072ef47f806238968a9bc73fc7972d1c6bbc
Damyan Ivanov <dam+mobileledger@ktnx.net>
2021-03-02 23:12:27 +0200

    add divider before fallback templates

--- app/src/main/java/net/ktnx/mobileledger/ui/templates/TemplateViewHolder.java
+++ app/src/main/java/net/ktnx/mobileledger/ui/templates/TemplateViewHolder.java
@@ -17,46 +17,68 @@
 
 package net.ktnx.mobileledger.ui.templates;
 
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import net.ktnx.mobileledger.R;
 import net.ktnx.mobileledger.databinding.TemplateListTemplateItemBinding;
+import net.ktnx.mobileledger.databinding.TemplatesFallbackDividerBinding;
 import net.ktnx.mobileledger.db.TemplateHeader;
 
-class TemplateViewHolder extends RecyclerView.ViewHolder {
-    final TemplateListTemplateItemBinding b;
-    public TemplateViewHolder(@NonNull TemplateListTemplateItemBinding binding) {
-        super(binding.getRoot());
-        b = binding;
+abstract class BaseTemplateViewHolder extends RecyclerView.ViewHolder {
+    public BaseTemplateViewHolder(@NonNull View itemView) {
+        super(itemView);
     }
-    public void bindToItem(TemplateHeader item) {
-        b.templateName.setText(item.getName());
-        b.templateName.setOnClickListener(
-                v -> ((TemplatesActivity) v.getContext()).onEditTemplate(item.getId()));
-        b.templateName.setOnLongClickListener((v) -> {
-            TemplatesActivity activity = (TemplatesActivity) v.getContext();
-            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            final String templateName = item.getName();
-            builder.setTitle(templateName);
-            builder.setItems(R.array.templates_ctx_menu, (dialog, which) -> {
-                if (which == 0) { // edit
-                    activity.onEditTemplate(item.getId());
-                }
-                else if (which == 1) { // duplicate
-                    activity.onDuplicateTemplate(item.getId());
-                }
-                else if (which == 2) { // delete
-                    activity.onDeleteTemplate(item.getId());
-                }
-                else {
-                    throw new RuntimeException(String.format("Unknown menu item id (%d)", which));
-                }
-                dialog.dismiss();
+    abstract void bindToItem(TemplatesRecyclerViewAdapter.BaseTemplateItem item);
+    static class TemplateDividerViewHolder extends BaseTemplateViewHolder {
+        public TemplateDividerViewHolder(@NonNull TemplatesFallbackDividerBinding binding) {
+            super(binding.getRoot());
+        }
+        @Override
+        void bindToItem(TemplatesRecyclerViewAdapter.BaseTemplateItem item) {
+            // nothing
+        }
+    }
+
+    static class TemplateViewHolder extends BaseTemplateViewHolder {
+        final TemplateListTemplateItemBinding b;
+        public TemplateViewHolder(@NonNull TemplateListTemplateItemBinding binding) {
+            super(binding.getRoot());
+            b = binding;
+        }
+        @Override
+        public void bindToItem(TemplatesRecyclerViewAdapter.BaseTemplateItem baseItem) {
+            TemplateHeader item = ((TemplatesRecyclerViewAdapter.TemplateItem) baseItem).template;
+            b.templateName.setText(item.getName());
+            b.templateName.setOnClickListener(
+                    v -> ((TemplatesActivity) v.getContext()).onEditTemplate(item.getId()));
+            b.templateName.setOnLongClickListener((v) -> {
+                TemplatesActivity activity = (TemplatesActivity) v.getContext();
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                final String templateName = item.getName();
+                builder.setTitle(templateName);
+                builder.setItems(R.array.templates_ctx_menu, (dialog, which) -> {
+                    if (which == 0) { // edit
+                        activity.onEditTemplate(item.getId());
+                    }
+                    else if (which == 1) { // duplicate
+                        activity.onDuplicateTemplate(item.getId());
+                    }
+                    else if (which == 2) { // delete
+                        activity.onDeleteTemplate(item.getId());
+                    }
+                    else {
+                        throw new RuntimeException(
+                                String.format("Unknown menu item id (%d)", which));
+                    }
+                    dialog.dismiss();
+                });
+                builder.show();
+                return true;
             });
-            builder.show();
-            return true;
-        });
+        }
     }
 }
--- app/src/main/java/net/ktnx/mobileledger/ui/templates/TemplatesRecyclerViewAdapter.java
+++ app/src/main/java/net/ktnx/mobileledger/ui/templates/TemplatesRecyclerViewAdapter.java
@@ -26,42 +26,65 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import net.ktnx.mobileledger.databinding.TemplateListTemplateItemBinding;
+import net.ktnx.mobileledger.databinding.TemplatesFallbackDividerBinding;
 import net.ktnx.mobileledger.db.TemplateHeader;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TemplatesRecyclerViewAdapter extends RecyclerView.Adapter<TemplateViewHolder> {
-    private final AsyncListDiffer<TemplateHeader> listDiffer;
+public class TemplatesRecyclerViewAdapter extends RecyclerView.Adapter<BaseTemplateViewHolder> {
+    private static final int ITEM_TYPE_TEMPLATE = 1;
+    private static final int ITEM_TYPE_DIVIDER = 2;
+    private final AsyncListDiffer<BaseTemplateItem> listDiffer;
+
     public TemplatesRecyclerViewAdapter() {
-        listDiffer = new AsyncListDiffer<>(this, new DiffUtil.ItemCallback<TemplateHeader>() {
+        listDiffer = new AsyncListDiffer<>(this, new DiffUtil.ItemCallback<BaseTemplateItem>() {
             @Override
-            public boolean areItemsTheSame(@NotNull TemplateHeader oldItem,
-                                           @NotNull TemplateHeader newItem) {
-                return oldItem.getId()
-                              .equals(newItem.getId());
+            public boolean areItemsTheSame(
+                    @NotNull TemplatesRecyclerViewAdapter.BaseTemplateItem oldItem,
+                    @NotNull TemplatesRecyclerViewAdapter.BaseTemplateItem newItem) {
+                return oldItem.getId() == newItem.getId();
             }
             @Override
-            public boolean areContentsTheSame(@NotNull TemplateHeader oldItem,
-                                              @NotNull TemplateHeader newItem) {
+            public boolean areContentsTheSame(
+                    @NotNull TemplatesRecyclerViewAdapter.BaseTemplateItem oldItem,
+                    @NotNull TemplatesRecyclerViewAdapter.BaseTemplateItem newItem) {
                 return oldItem.equals(newItem);
             }
         });
     }
     @NonNull
     @Override
-    public TemplateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        TemplateListTemplateItemBinding b =
-                TemplateListTemplateItemBinding.inflate(LayoutInflater.from(parent.getContext()),
-                        parent, false);
-
-        return new TemplateViewHolder(b);
+    public BaseTemplateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        switch (viewType) {
+            case ITEM_TYPE_TEMPLATE:
+                TemplateListTemplateItemBinding b =
+                        TemplateListTemplateItemBinding.inflate(inflater, parent, false);
+                return new BaseTemplateViewHolder.TemplateViewHolder(b);
+            case ITEM_TYPE_DIVIDER:
+                return new BaseTemplateViewHolder.TemplateDividerViewHolder(
+                        TemplatesFallbackDividerBinding.inflate(inflater, parent, false));
+            default:
+                throw new RuntimeException("Can't handle " + viewType);
+        }
     }
     @Override
-    public void onBindViewHolder(@NonNull TemplateViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull BaseTemplateViewHolder holder, int position) {
         holder.bindToItem(listDiffer.getCurrentList()
                                     .get(position));
+    }
+    @Override
+    public int getItemViewType(int position) {
+        BaseTemplateItem item = getItem(position);
+        if (item instanceof TemplateItem)
+            return ITEM_TYPE_TEMPLATE;
+        if (item instanceof TemplateDivider)
+            return ITEM_TYPE_DIVIDER;
+
+        throw new RuntimeException("Can't handle " + item);
     }
     @Override
     public int getItemCount() {
@@ -69,6 +92,52 @@ public class TemplatesRecyclerViewAdapter extends RecyclerView.Adapter<TemplateV
                          .size();
     }
     public void setTemplates(List<TemplateHeader> newList) {
-        listDiffer.submitList(newList);
+        List<BaseTemplateItem> itemList = new ArrayList<>();
+
+        boolean reachedFallbackItems = false;
+
+        for (TemplateHeader item : newList) {
+            if (!reachedFallbackItems && item.isFallback()) {
+                itemList.add(new TemplateDivider());
+                reachedFallbackItems = true;
+            }
+            itemList.add(new TemplateItem(item));
+        }
+
+        listDiffer.submitList(itemList);
+    }
+    public BaseTemplateItem getItem(int position) {
+        return listDiffer.getCurrentList()
+                         .get(position);
+    }
+
+    static abstract class BaseTemplateItem {
+        abstract long getId();
+
+        abstract boolean equals(BaseTemplateItem other);
+    }
+
+    static class TemplateItem extends BaseTemplateItem {
+        final TemplateHeader template;
+        TemplateItem(TemplateHeader template) {this.template = template;}
+        @Override
+        long getId() {
+            return template.getId();
+        }
+        @Override
+        boolean equals(BaseTemplateItem other) {
+            return template.equals(((TemplateItem) other).template);
+        }
+    }
+
+    static class TemplateDivider extends BaseTemplateItem {
+        @Override
+        long getId() {
+            return -1;
+        }
+        @Override
+        boolean equals(BaseTemplateItem other) {
+            return true;
+        }
     }
 }
